3a4711e4e5827551fabd810fc0e4d7499297e597
Leonciny <albeleo75@gmail.com>
2023-02-02 23:38:52 +0100

    dasdsad

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,37 @@ import reactLogo from './assets/react.svg'
 import './App.css'
 import Scheduler from './components/Scheduler'
 import { PRIMARY_COLOR, SECONDARY_COLOR, ACCENT_COLOR, CELL_ACCENT_COLOR, CELL_PRIMARY_COLOR, CELL_SECONDATY_COLOR } from './Settings'
+import { Subject, Teacher } from './Interfaces'
+
+type HourData = {
+  day      : number
+  fromHour : number
+  toHour   : number
+  teachers : Teacher[]
+  subject  : Subject
+}
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const data = new Array<HourData>()
+  data.push(
+    {
+      day: 1,
+      fromHour: 0,
+      toHour: 1,
+      teachers: [],
+      subject: {
+        Name : "Ginnastica"
+      }
+    }
+  )
 
   return (
     <Scheduler 
       height={"100%"}
       width={"100%"}
-      data={[]}
-      hourTabWidth={'10%'}    
+      data={data}
+      hourTabWidth={'15%'}    
       dayTabHeight={'10%'}
       accent_color={ACCENT_COLOR}
       primary_color={PRIMARY_COLOR}
--- src/components/Scheduler.tsx
+++ src/components/Scheduler.tsx
@@ -5,6 +5,7 @@ import { Subject, Teacher } from "../Interfaces"
 type HEXColorString  = string
 type RGBColorString  = string | [ number, number, number ]
 type RGBAColorString = string | [ number, number, number, number ]
+type AnyKindOfColor  = HEXColorString | RGBAColorString | RGBAColorString
 
 type HourData = {
     day      : number
@@ -41,20 +42,32 @@ type Props = {
     timeSettings : TimeSettings
 }
 
+
+
 const FALLBACK_UNIT_TYPE = "px",
       EVER               = true,
+      SPAN_BORDER_SIZE   = 2,
+      CELL_BORDER_SIZE   = 1,
       HOUR_FORMATTER     = Intl.NumberFormat(undefined, {minimumIntegerDigits : 2})
 
-const TODAY        = new Date(),
-      CURRENT_WEEK = new Date()
+const TODAY         = new Date(),
+      CURRENT_WEEK  = new Date(),
+      MONDAY_OFFSET = 1,
+      DAYS          = [
+          "Lunedì",
+          "Martedì",
+          "Mercoledì",
+          "Giovedì",
+          "Venerdì"
+      ]
 
 CURRENT_WEEK.setHours(0)
 CURRENT_WEEK.setMilliseconds(0)
 CURRENT_WEEK.setSeconds(0)
 CURRENT_WEEK.setMinutes(0)
-CURRENT_WEEK.setDate(
-    CURRENT_WEEK.getDate() - CURRENT_WEEK.getDay()
-)
+CURRENT_WEEK.setDate( CURRENT_WEEK.getDate() - CURRENT_WEEK.getDay() )
+
+
 
 const extractColor : ( color : RGBColorString | RGBAColorString | HEXColorString ) => string = color => 
     ( typeof color === "string" ) 
@@ -70,48 +83,50 @@ const extractMaybeProp : ( prop : undefined | number | string ) => string = prop
             ? prop
             : `${prop}${FALLBACK_UNIT_TYPE}`
 
+
+
 const DayView = ({ currentWeek, height, width, accent_color, accent_text_color } : { currentWeek : Date, height : string, width : string, accent_color : string, accent_text_color : string  } ) => {
+    
     let temp_data = new Date(currentWeek)
     temp_data.setDate(temp_data.getDate() + 1)
+
     const getDataAndIncrement = () => {
         let ret = temp_data.getDate();
         temp_data.setDate(temp_data.getDate() + 1)
         return ret
     }
+
+    const getSpan = ( day : number, month : number ) => {
+        const IS_TODAY =  TODAY.getDate()  === day 
+                       && TODAY.getMonth() === month
+        
+        return (
+            <span style={ 
+                IS_TODAY ? {
+                    backgroundColor : accent_color,
+                    color           : accent_text_color,
+                    border          : `${SPAN_BORDER_SIZE}px solid ${accent_color}`
+                } : {
+                    border          : `${SPAN_BORDER_SIZE}px solid ${accent_color}`
+                } 
+            }>
+                {day}/{month + 1}
+            </span>
+        )
+    }
+
     return (
     <div className={Style.schedulerDayContainer} style={{width}}>
-        <div className={Style.schedulerDay}>
-            <div>
-                <span style={{backgroundColor:accent_color, color: accent_text_color}}>{getDataAndIncrement()}/{temp_data.getMonth() + 1}</span>
+        {DAYS.map( el => (
+            <div className={Style.schedulerDay} key={el}>
+                <div>
+                    {getSpan(getDataAndIncrement(),temp_data.getMonth())}
+                </div>
+                <div>
+                    <p>{el}</p>
+                </div>
             </div>
-            <div>
-                <p>Lunedì</p>
-            </div>
-        </div>
-        <div className={Style.schedulerDay}>
-            <div>
-                <span style={{backgroundColor:accent_color, color: accent_text_color}}>{getDataAndIncrement()}/{temp_data.getMonth() + 1}</span>
-            </div>
-            <p>Martedì</p>
-        </div>
-        <div className={Style.schedulerDay}>
-            <div>
-                <span style={{backgroundColor:accent_color, color: accent_text_color}}>{getDataAndIncrement()}/{temp_data.getMonth() + 1}</span>
-            </div>
-            <p>Mercoledì</p>
-        </div>
-        <div className={Style.schedulerDay}>
-            <div>
-                <span style={{backgroundColor:accent_color, color: accent_text_color}}>{getDataAndIncrement()}/{temp_data.getMonth() + 1}</span>
-            </div>
-            <p>Giovedì</p>
-        </div>
-        <div className={Style.schedulerDay}>
-            <div>
-                <span style={{backgroundColor:accent_color, color: accent_text_color}}>{getDataAndIncrement()}/{temp_data.getMonth() + 1}</span>
-            </div>
-            <p>Venerdì</p>
-        </div>
+        ))}
     </div>
 )}
 
@@ -138,16 +153,24 @@ const HourDisplay = ({ timeSettings, hourTabWidth, numberOfModules } : { timeSet
         <div className={Style.hourDisplayContainer} style={{ width : hourTabWidth }}>
             {modules.map( mod => ( 
             <div key={mod} className={Style.hourDisplay}>
-                <p className={Style.hourLabel}>{mod}</p>
-            </div> 
+                <p className={Style.hourLabel}
+                    style={{ transform : `translate(0, calc(${CELL_BORDER_SIZE / 2}px + var(--hour-display-font-size)/2))`}}
+                >{mod}</p>
+            </div>
             ))}
+            <div className={Style.hourDisplay}></div>
         </div>
     )
 }
 
-const HourGridDisplay = ({ data, cellProps, numberOfModules, width } : { data : HourData[], cellProps : CellProps, numberOfModules : number, width : string }) => {
+const HourGridDisplay = ({ data, totalHeight, dayTabHeight, cellProps, borderColor, numberOfModules, popUpCol, width } : { data : HourData[], popUpCol : string, totalHeight : string, cellProps : CellProps, numberOfModules : number, dayTabHeight : string, width : string, borderColor: string }) => {
     
+    const CELL_PRIMARY_COL   = extractColor(cellProps.primary_color   ),
+          CELL_SECONDARY_COL = extractColor(cellProps.secondary_color ),
+          CELL_TEXT_COL      = extractColor(cellProps.text_color      )
+
     let actual_data = new Array<HourData>()
+    let foundFlag = false;
     data.forEach( el => { 
         for( let elem of actual_data ) {
             if (   elem.day      == el.day 
@@ -155,27 +178,65 @@ const HourGridDisplay = ({ data, cellProps, numberOfModules, width } : { data : 
                 && elem.toHour   == el.toHour
                 && elem.subject  == el.subject
             ) elem.teachers.push( ...el.teachers )
-            else actual_data.push( el )
+            else {
+                foundFlag = true
+                actual_data.push( el )
+            }
         }
+        if(!foundFlag)actual_data.push( el )
+        foundFlag = false
     })
-
+    console.log(actual_data)
     const getItemForCell = (index : number) => {
-        const DAY  = index % 5,
+
+        const [ showPopUp, setShowPopUp ] = useState(false),
+              DAY  = index % 5,
               HOUR = ~~ ( index / 5 ),
-              DATA = actual_data.find( hour => hour.day === DAY && hour.fromHour === HOUR )
+              DATA = actual_data.find( hour => (hour.day - MONDAY_OFFSET) === DAY && hour.fromHour === HOUR )
+
         return DATA === undefined ? (
-            <div className={Style.emptyCell} />
+            <div className={Style.HourGridCellData} />
         ) : (
-            <div className={Style.HourGridCellData}>
-                <p>{ DATA.subject.Name }</p>
+            <div className={Style.HourGridCellDataWrap}>
+                <div 
+                    className={Style.HourGridCellData} 
+                    style={{
+                        backgroundColor : CELL_PRIMARY_COL,
+                        color           : CELL_TEXT_COL,
+                    }} 
+                    onMouseOver = { ev => ev.currentTarget.style.backgroundColor = CELL_SECONDARY_COL }
+                    onMouseOut  = { ev => ev.currentTarget.style.backgroundColor = CELL_PRIMARY_COL   }
+                    onClick     = { ev => setShowPopUp(true) }
+                >
+                    <p>{ DATA.subject.Name }</p>
+                </div>
+                <div 
+                    className={Style.popUpWrapper}
+                    style={{
+                        display : showPopUp ? "grid" : "none", 
+                        backgroundColor : popUpCol,
+                        width   : `calc(${width} / ${DAYS.length} - 2px)`,
+                        height  : `calc( calc(${totalHeight} - ${dayTabHeight}) / ${numberOfModules})`
+                    }}
+                >
+                    ds
+                </div>
             </div>
         )
     }
 
     return (
-        <div className={Style.HourGridContainer} style={{width}}>
-            {new Array( numberOfModules * 5 ).fill(undefined).map( ( el, index ) => (
-                <div className={Style.HourGridCell} key={index}>
+        <div className={Style.HourGridContainer} style={{
+            width,
+            gridTemplateColumns : `1fr `.repeat(DAYS.length),
+            gridTemplateRows    : `1fr `.repeat(numberOfModules)
+        }}>
+            {new Array( numberOfModules * DAYS.length ).fill(undefined).map( ( el, index ) => (
+                <div className={Style.HourGridCell} key={index} style={{
+                    border     : `${CELL_BORDER_SIZE}px solid ${borderColor}`,
+                    borderTop  : !(~~(index/DAYS.length)) ? `${CELL_BORDER_SIZE}px solid ${borderColor}` : "0",
+                    borderLeft : !(index%DAYS.length)     ? `${CELL_BORDER_SIZE}px solid ${borderColor}` : "0"
+                }}>
                     {getItemForCell(index)}
                 </div>
             ))}
@@ -216,8 +277,18 @@ export default ({height, width, hourTabWidth, dayTabHeight, data, accent_color, 
         </div>
         <div className={Style.bottomWrap}>
             <HourDisplay hourTabWidth={hourTabWidth} timeSettings={timeSettings} numberOfModules={NUMBER_OF_MODULES}/>
-            <HourGridDisplay data={data} cellProps={cellProps} numberOfModules={NUMBER_OF_MODULES} width={`calc(100% - ${hourTabWidth})`}/>
+            <HourGridDisplay 
+                dayTabHeight={typeof dayTabHeight === "number" ? `${dayTabHeight}px` : dayTabHeight} 
+                borderColor={actualSecCol} 
+                popUpCol={actualPriCol}
+                data={data} 
+                cellProps={cellProps} 
+                numberOfModules={NUMBER_OF_MODULES}
+                totalHeight={actualH} 
+                width={`calc(100% - ${hourTabWidth})`}
+            />
         </div>
+        
     </div>
     )
 }
